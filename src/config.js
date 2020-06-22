55c13a170f391ba52404d0f4ff1fde567cac5a3f
snelluri <sivaram.nelluri@gmail.com>
2020-06-22 01:08:23 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "notes-app-uploads-nelluri"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://eyjw8u3vri.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_LUUKbFnba",
+    APP_CLIENT_ID: "2lrbto9m7e3l87c3d5gnpf6em0",
+    IDENTITY_POOL_ID: "us-west-2:58b7ac18-d33f-46ac-8c5f-a725c192e28b"
+  }
+};
